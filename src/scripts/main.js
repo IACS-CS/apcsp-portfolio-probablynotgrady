b3a8566f133a2cdb24f756135e3efa33fb26216f
probablynotgrady <ipodidiot1000@gmail.com>
2025-09-25 18:44:30 +0000

    v0.0.1, added picocss and now everything is very very upset

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 import "../styles/styles.css";
 import { startRouter } from "./router.js";
+import "picocss/pico/css/pico.min.css";
 
 /* Start the router (this lets us show different pages -- see 
 router.js) */
