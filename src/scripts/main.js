9858a942f9a45af7dc501da9994269f8778ebe60
probablynotgrady <ipodidiot1000@gmail.com>
2025-10-02 18:33:18 +0000

    made new TI block, but now the website is just a useless TI block without any way to navigate pages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,14 @@
+import { TextInterface } from "text-interface";
 import "../styles/styles.css";
 import { startRouter } from "./router.js";
-import "picocss/pico/css/pico.min.css";
+import "@picocss/pico/css/pico.min.css";
 
 /* Start the router (this lets us show different pages -- see 
 router.js) */
 startRouter();
+
+let ti = new TextInterface(document.querySelector("#terminal"));
+let response = await ti.prompt("guest@Chase-DeLuca-Portfolio ~ %");
+if (response === "ls") {
+  ti.output("Documents  Downloads  Music  Pictures  Videos");
+}
