b7a5b4aa3107dae9a7f21e5362e953e9f42b057e
probablynotgrady <ipodidiot1000@gmail.com>
2025-10-14 16:22:34 +0000

    v1.0.2b, changed vite.config.js to hopefully fix issue.

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,18 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: ".",
   publicDir: "public",
-  base: process.env.NODE_ENV === "production" ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1] || 'apcsp-portfolio-template'}/` : "/",
+  base:
+    process.env.NODE_ENV === "production"
+      ? `/${
+          process.env.GITHUB_REPOSITORY?.split("/")[1] ||
+          "apcsp-portfolio-template"
+        }/`
+      : "/",
   build: {
     outDir: "dist",
     emptyOutDir: true,
     minify: false,
+    target: "esnext",
   },
   server: {
     open: true,
