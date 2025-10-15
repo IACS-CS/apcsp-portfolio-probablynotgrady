daede7b480be165da17397620a57af6664cba9c8
probablynotgrady <ipodidiot1000@gmail.com>
2025-10-15 15:43:18 +0000

    v1.0.4, changed path locations

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -7,7 +7,7 @@ import { renderCitations } from "./pages/citations.js";
 /* route takes a path and chooses which page to render.
 It is called each time the user clicks on a navigation link */
 export function route(path) {
-  if (path === "" || path === "/") {
+  if (path === "apcsp-portfolio-probablynotgrady/" || path === "/") {
     // show terminal on home page
     document.getElementById("terminal-container").classList.add("active");
     // hide "app"
@@ -18,11 +18,11 @@ export function route(path) {
     document.getElementById("terminal-container").classList.remove("active");
     // show "app"
     document.getElementById("app").classList.add("active");
-    if (path === "/binary-search") {
+    if (path === "/apcsp-portfolio-probablynotgrady/binary-search") {
       return renderBinarySearch();
-    } else if (path === "/portfolio-project") {
+    } else if (path === "/apcsp-portfolio-probablynotgrady/portfolio-project") {
       return renderPortfolio();
-    } else if (path === "/citations") {
+    } else if (path === "/apcsp-portfolio-probablynotgrady/citations") {
       return renderCitations();
     } else {
       return `<h1>404 Not Found</h1><a href="#/">Back Home</a>`;
